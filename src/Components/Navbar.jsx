2b2d0bfd83826d24b815f4fc0a8369606676df88
abd-005 <abd.me7@gmail.com>
2025-10-21 22:26:28 +0600

    Navbar Added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import { Link, NavLink } from "react-router";
+import logo from "../assets/img/firebase-logo.png";
+import MyContainer from "./MyContainer";
+import MyLink from "./MyLink";
+import { use } from "react";
+// import { AuthContext } from "../context/AuthContext";
+import { toast } from "react-toastify";
+// import { ClockLoader } from "react-spinners";
+// import MyLink from "./MyLink";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    );
+//   const result = useContext(AuthContext);
+//   const { user, signoutUserFunc, setUser, loading, setLoading } =
+//     use(AuthContext);
+//   console.log(user);
+
+  const handleSignout = () => {
+//     signoutUserFunc()
+//       .then(() => {
+//         toast.success("Signout successful");
+//         setUser(null);
+//       })
+//       .catch((e) => {
+//         toast.error(e.message);
+//       });
+  console.log("HandleSignout");
+  };
+
+//   console.log(loading);
+
+  return (
+    <div className="bg-slate-100f py-2 border-b border-b-slate-300 ">
+      <MyContainer className="flex items-center justify-between">
+        <figure>
+          <img src={logo} className="w-[55px]" />
+        </figure>
+        <ul className="flex items-center gap-2">
+          <li>
+            <MyLink to={"/"}>Home</MyLink>
+          </li>
+          <li>
+            <MyLink to={"/about-us"}>About US</MyLink>
+          </li>
+          {(
+            <li>
+              <MyLink to={"/profile"}>Profile</MyLink>
+            </li>
+          )}
+        </ul>
+      </MyContainer>
+    </div>
+  );
 };
 
 export default Navbar;
